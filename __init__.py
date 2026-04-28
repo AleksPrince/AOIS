f7bcf1db013fd156a441b69c4515fc27c980d21a
AleksPrince <pro100aleksandrapro@gmail.com>
2026-04-28 10:28:27 +0300

    Delete __init__.py

--- __init__.py
+++
@@ -1,7 +0,0 @@
-
-from .core.logic_function import LogicFunction
-from .core.expression_processor import ExpressionProcessor
-from .analyzer import BooleanAnalyzer
-
-__version__ = "4.2.1"
-__all__ = ['BooleanAnalyzer', 'LogicFunction', 'ExpressionProcessor']
