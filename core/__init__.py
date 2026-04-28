c67cf8f45e201426f4b33de3d7445114957cef00
AleksPrince <pro100aleksandrapro@gmail.com>
2026-04-28 10:27:11 +0300

    Delete core directory

--- core/__init__.py
+++
@@ -1,5 +0,0 @@
-from .expression_processor import ExpressionProcessor
-from .truth_matrix import TruthMatrix
-from .logic_function import LogicFunction
-
-__all__ = ['ExpressionProcessor', 'TruthMatrix', 'LogicFunction']
