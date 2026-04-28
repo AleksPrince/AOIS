50e498ad62ab46245fe838dd29b6966c2c575410
AleksPrince <pro100aleksandrapro@gmail.com>
2026-04-28 10:22:51 +0300

    Delete lab2 directory

--- lab2/lab2/minimization/__init__.py
+++
@@ -1,5 +0,0 @@
-from .glue_minimizer import GlueMinimizer
-from .table_minimizer import TableMinimizer
-from .karnaugh_minimizer import KarnaughMinimizer
-
-__all__ = ['GlueMinimizer', 'TableMinimizer', 'KarnaughMinimizer']
